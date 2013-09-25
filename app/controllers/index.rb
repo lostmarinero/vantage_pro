1e802fa92181bb29e4ca9dba86834b83a39b932a
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-24 21:40:47 -0500

    added a little more styling to the main page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -14,7 +14,7 @@ get '/' do
   results.each do |image|
     new_image = Image.create(name: "image[:caption]",
             user_name: image[:user][:full_name],
-             taken_at: image[:created_time],
+             taken_at: Time.at(image[:created_time].to_i).to_datetime,
                   url: image[:images][:standard_resolution][:url])
 
     image[:tags].each do |tag|
