eed2e91b23e1aaa540ac9b2941ca7565ffcf4263
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-24 19:04:56 -0500

    paused auto refresh

--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
-   var time = new Date().getTime();
-     $(document.body).bind("mousemove keypress", function(e) {
-         time = new Date().getTime();
-     });
+// $(document).ready(function(){
+//    var time = new Date().getTime();
+//      $(document.body).bind("mousemove keypress", function(e) {
+//          time = new Date().getTime();
+//      });
 
-     function refresh() {
-         if(new Date().getTime() - time >= 3000) 
-             window.location.reload(true);
-         else 
-             setTimeout(refresh, 3000);
-     }
+//      function refresh() {
+//          if(new Date().getTime() - time >= 3000) 
+//              window.location.reload(true);
+//          else 
+//              setTimeout(refresh, 3000);
+//      }
 
-     setTimeout(refresh, 30000);
-});
+//      setTimeout(refresh, 30000);
+// });
